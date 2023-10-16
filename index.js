d71bf2a71bb72c724afbbcc4f653869094eec7a4
Katia <kushnir.ekaterina@gmail.com>
2023-10-16 17:19:09 +0200

    Añadida la funcion suma

---
+++ index.js
@@ -0,0 +1,26 @@
+function suma(num1, num2) {
+    let suma = num1 + num2;
+    return suma
+}
+
+function resta(num1, num2) {
+    let resta = num1 - num2;
+    return resta
+}
+
+function producto(num1, num2) {
+    let producto = num1 * num2;
+    return producto
+}
+
+function division(num1, num2) {
+    let division = num1 / num2;
+    return division
+}
+
+function cuadrado(num) {
+    let cuadrado = num * num;
+    return cuadrado
+}
+
+module.exports = {suma, resta, producto, division, cuadrado}
---
+++ run.js
@@ -0,0 +1,7 @@
+const myLib = require("./index");
+
+console.log(myLib.suma(2, 2));
+console.log(myLib.resta(4, 1));
+console.log(myLib.producto(3, 2));
+console.log(myLib.division(9, 3));
+console.log(myLib.cuadrado(9));
